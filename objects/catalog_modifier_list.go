08fd430cc78105cb598f130201c752f54068c6d8
James Benze <benzejaa@gmail.com>
2021-04-10 16:35:58 -0400

    More enum fixes

--- objects/catalog_modifier_list.go
+++ objects/catalog_modifier_list.go
@@ -1,10 +1,17 @@
 package objects
 
+type CatalogModifierListSelectionType string
+
+const (
+	CatalogModifierListSelectionTypeSingle   CatalogModifierListSelectionType = "SINGLE"
+	CatalogModifierListSelectionTypeMultiple CatalogModifierListSelectionType = "MULTIPLE"
+)
+
 type CatalogModifierList struct {
-	Name          string           `json:"name,omitempty"`
-	Ordinal       int              `json:"ordinal,omitempty"`
-	SelectionType string           `json:"selection_type,omitempty"`
-	Modifiers     []*CatalogObject `json:"modifiers,omitempty"`
+	Name          string                           `json:"name,omitempty"`
+	Ordinal       int                              `json:"ordinal,omitempty"`
+	SelectionType CatalogModifierListSelectionType `json:"selection_type,omitempty"`
+	Modifiers     []*CatalogObject                 `json:"modifiers,omitempty"`
 }
 
 func (*CatalogModifierList) isCatalogObjectType() {}
