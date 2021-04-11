b5d3ad2dc519fd4fa372584894b54f1d07ab21c9
James Benze <benzejaa@gmail.com>
2021-04-11 07:33:50 -0400

    Refactor product set
    Product set has a lot of fields that are mutually exlusive, this uses
    variants to represent that exclusion in the type itself.

--- objects/catalog_product_set.go
+++ objects/catalog_product_set.go
@@ -1,8 +1,48 @@
 package objects
 
-type CatalogProductSet struct {
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type CatalogProductSetQuantity interface {
+	isCatalogProductSetQuantity()
+}
+
+type CatalogProductSetQuantityExact struct {
+	Amount int
+}
+
+type CatalogProductSetQuantityRange struct {
+	Min int
+	Max int
+}
+
+func (*CatalogProductSetQuantityExact) isCatalogProductSetQuantity() {}
+func (*CatalogProductSetQuantityRange) isCatalogProductSetQuantity() {}
+
+type CatalogProductSetProduct interface {
+	isCatalogProductSetProduct()
+}
+
+type CatalogProductSetAllProducts struct{}
+
+type CatalogProductSetAllIDs struct {
+	IDs []string
+}
+
+type CatalogProductSetAnyIDs struct {
+	IDs []string
+}
+
+func (*CatalogProductSetAllProducts) isCatalogProductSetProduct() {}
+func (*CatalogProductSetAllIDs) isCatalogProductSetProduct()      {}
+func (*CatalogProductSetAnyIDs) isCatalogProductSetProduct()      {}
+
+type catalogProductSet struct {
+	*catalogProductSetAlias
 	AllProducts   bool     `json:"all_products,omitempty"`
-	Name          string   `json:"name,omitempty"`
 	ProductIDsAll []string `json:"product_ids_all,omitempty"`
 	ProductIDsAny []string `json:"product_ids_any,omitempty"`
 	QuantityExact int      `json:"quantity_exact,omitempty"`
@@ -10,4 +50,83 @@ type CatalogProductSet struct {
 	QuantityMin   int      `json:"quantity_min,omitempty"`
 }
 
+type catalogProductSetAlias CatalogProductSet
+
+type CatalogProductSet struct {
+	Name     string                    `json:"name,omitempty"`
+	Products CatalogProductSetProduct  `json:"-"`
+	Quantity CatalogProductSetQuantity `json:"-"`
+}
+
 func (*CatalogProductSet) isCatalogObjectType() {}
+
+func (c *CatalogProductSet) MarshalJSON() ([]byte, error) {
+	jsonType := &catalogProductSet{
+		catalogProductSetAlias: (*catalogProductSetAlias)(c),
+	}
+
+	switch t := c.Products.(type) {
+	case *CatalogProductSetAllProducts:
+		jsonType.AllProducts = true
+	case *CatalogProductSetAllIDs:
+		jsonType.ProductIDsAll = t.IDs
+	case *CatalogProductSetAnyIDs:
+		jsonType.ProductIDsAny = t.IDs
+	default:
+		return nil, fmt.Errorf("unknown product type found")
+	}
+
+	switch t := c.Quantity.(type) {
+	case *CatalogProductSetQuantityExact:
+		jsonType.QuantityExact = t.Amount
+	case *CatalogProductSetQuantityRange:
+		jsonType.QuantityMin = t.Min
+		jsonType.QuantityMax = t.Max
+	}
+
+	b, err := json.Marshal(jsonType)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling catalog product set: %w", err)
+	}
+
+	return b, nil
+}
+
+func (c *CatalogProductSet) UnmarshalJSON(b []byte) error {
+	jsonType := &catalogProductSet{
+		catalogProductSetAlias: (*catalogProductSetAlias)(c),
+	}
+
+	err := json.Unmarshal(b, &jsonType)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling catalog product set: %w", err)
+	}
+
+	if jsonType.QuantityExact != 0 {
+		c.Quantity = &CatalogProductSetQuantityExact{
+			Amount: jsonType.QuantityExact,
+		}
+	} else {
+		c.Quantity = &CatalogProductSetQuantityRange{
+			Min: jsonType.QuantityMin,
+			Max: jsonType.QuantityMax,
+		}
+	}
+
+	switch {
+	case jsonType.AllProducts:
+		c.Products = &CatalogProductSetAllProducts{}
+	case jsonType.ProductIDsAll != nil:
+		c.Products = &CatalogProductSetAllIDs{
+			IDs: jsonType.ProductIDsAll,
+		}
+	case jsonType.ProductIDsAny != nil:
+		c.Products = &CatalogProductSetAnyIDs{
+			IDs: jsonType.ProductIDsAny,
+		}
+	default:
+		return errors.New("no product specifier set")
+	}
+
+	return nil
+}
