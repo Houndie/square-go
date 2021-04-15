9ef2f9308df709b9a7acd2e3599028042b2ab43f
James Benze <benzejaa@gmail.com>
2021-04-15 06:51:11 -0400

    Add QuickAmountSettingsOption type

--- objects/catalog_quick_amounts_settings.go
+++ objects/catalog_quick_amounts_settings.go
@@ -1,9 +1,17 @@
 package objects
 
+type CatalogQuickAmountSettingsOption string
+
+const (
+	CatalogQuickAmountSettingsOptionDisabled CatalogQuickAmountSettingsOption = "DISABLED"
+	CatalogQuickAmountSettingsOptionAuto     CatalogQuickAmountSettingsOption = "AUTO"
+	CatalogQuickAmountSettingsOptionManual   CatalogQuickAmountSettingsOption = "MANUAL"
+)
+
 type CatalogQuickAmountsSettings struct {
-	Option                 string                `json:"option,omitempty"`
-	Amounts                []*CatalogQuickAmount `json:"amounts,omitempty"`
-	EligibleForAutoAmounts bool                  `json:"eligible_for_auto_amounts,omitempty"`
+	Option                 CatalogQuickAmountSettingsOption `json:"option,omitempty"`
+	Amounts                []*CatalogQuickAmount            `json:"amounts,omitempty"`
+	EligibleForAutoAmounts bool                             `json:"eligible_for_auto_amounts,omitempty"`
 }
 
 func (*CatalogQuickAmountsSettings) isCatalogObjectType() {}
