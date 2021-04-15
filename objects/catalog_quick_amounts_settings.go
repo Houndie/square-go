1b8587305cbdf2b8f7878d28bfac0e1b07306b89
James Benze <benzejaa@gmail.com>
2021-04-15 07:12:54 -0400

    s/AmountSettings/AmountsSettings/g
    (fixing a repeated typo to match square)

--- objects/catalog_quick_amounts_settings.go
+++ objects/catalog_quick_amounts_settings.go
@@ -1,17 +1,17 @@
 package objects
 
-type CatalogQuickAmountSettingsOption string
+type CatalogQuickAmountsSettingsOption string
 
 const (
-	CatalogQuickAmountSettingsOptionDisabled CatalogQuickAmountSettingsOption = "DISABLED"
-	CatalogQuickAmountSettingsOptionAuto     CatalogQuickAmountSettingsOption = "AUTO"
-	CatalogQuickAmountSettingsOptionManual   CatalogQuickAmountSettingsOption = "MANUAL"
+	CatalogQuickAmountsSettingsOptionDisabled CatalogQuickAmountsSettingsOption = "DISABLED"
+	CatalogQuickAmountsSettingsOptionAuto     CatalogQuickAmountsSettingsOption = "AUTO"
+	CatalogQuickAmountsSettingsOptionManual   CatalogQuickAmountsSettingsOption = "MANUAL"
 )
 
 type CatalogQuickAmountsSettings struct {
-	Option                 CatalogQuickAmountSettingsOption `json:"option,omitempty"`
-	Amounts                []*CatalogQuickAmount            `json:"amounts,omitempty"`
-	EligibleForAutoAmounts bool                             `json:"eligible_for_auto_amounts,omitempty"`
+	Option                 CatalogQuickAmountsSettingsOption `json:"option,omitempty"`
+	Amounts                []*CatalogQuickAmount             `json:"amounts,omitempty"`
+	EligibleForAutoAmounts bool                              `json:"eligible_for_auto_amounts,omitempty"`
 }
 
 func (*CatalogQuickAmountsSettings) isCatalogObjectType() {}
